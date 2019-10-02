3d93b26da4dad020854dfb8c0415b234556a111e
marionbosc <bosc@cshl.edu>
2019-10-02 16:04:46 -0400

    Correct to make sure that the size of StartEasyTrial field is not > to Custom.nTrials

--- set_StartEasyTrial_field.m
+++ set_StartEasyTrial_field.m
@@ -6,8 +6,13 @@ function SessionData = set_StartEasyTrial_field(SessionData)
 
 if ~isfield(SessionData.Custom , 'StartEasyTrial') || size(SessionData.Custom.StartEasyTrial,2) < size(SessionData.Custom.ChoiceLeft,2)
     if SessionData.DayvsWeek == 1
-        SessionData.Custom.StartEasyTrial(1:SessionData.Settings.GUI.StartEasyTrials)= 1;
-        SessionData.Custom.StartEasyTrial(SessionData.Settings.GUI.StartEasyTrials+1:max(SessionData.Custom.TrialNumber))= 0;    
+        for trial = 1 : SessionData.nTrials
+            if trial < SessionData.Settings.GUI.StartEasyTrials
+                SessionData.Custom.StartEasyTrial(trial)= 1;
+            else
+                SessionData.Custom.StartEasyTrial(trial)= 0; 
+            end
+        end
     elseif SessionData.DayvsWeek == 2
         SessionData.Custom.StartEasyTrial(1:size(SessionData.Custom.ChoiceLeft,2))= 0;
         for session = unique(SessionData.Custom.Session)
